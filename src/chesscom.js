9e56af00b4dcd1cc19d186a5dde3970fce745255
Matteo Golinelli <matteogolinelli97@gmail.com>
2024-08-16 19:30:06 +0200

    Accomodate chess.com HTML changes and improve buttons placement

--- src/chesscom.js
+++ src/chesscom.js
@@ -31,7 +31,7 @@ async function openTab() {
         await sleep(0.5);
 
     // Get the "Timestamps" setting from the extension
-    browser.storage.sync.get("timestamps", async function(result) {
+    browser.storage.sync.get("timestamps", async function (result) {
         if (result.timestamps) {
             while (document.getElementsByClassName("circle-clock icon-font-chess share-menu-tab-pgn-icon")[0] == null)
                 await sleep(0.5);
@@ -42,7 +42,6 @@ async function openTab() {
     });
 
     const pgn = document.getElementsByClassName("share-menu-tab-image-component")[0].getAttribute("pgn");
-    // console.log("PGN: " + pgn);
     try {
         // Send a message to the background script
         browser.runtime.sendMessage(pgn);
@@ -51,55 +50,83 @@ async function openTab() {
     }
 }
 
+function createLichessAnalysisButton(id = 'lichess-analysis-button') {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.id = id;
+    button.className = 'ui_v5-button-component ui_v5-button-primary ui_v5-button-large ui_v5-button-full';
+    button.innerHTML = '<span class="lichess-icon"></span><span> Analyse on Lichess</span>';
+
+    button.setAttribute('style', 'grid-column: span 2;');
+
+    button.addEventListener('click', openTab);
+
+    return button;
+}
+
 // Wait for an element to be added to the page
 const onMutation = () => {
-    var element = document.getElementsByClassName('new-game-buttons-component')[0];
-    if (element == null) {
-        element = document.getElementsByClassName('daily-game-footer-game-over')[0];
-    }
-    if (element == null) {
-        element = document.getElementsByClassName('quick-analysis-buttons')[0];
-    }
-    if (element == null) {
-        element = document.getElementsByClassName('game-controls-view-component')[0];
-    }
+    // Add the Lichess Analysis button to the game-over popup, if it doesn't exist yet
+    if (!document.getElementById('lichess-game-over-analysis-button')) {
+        // If game-over-modal-buttons element exists, add the button there
+        var element = document.getElementsByClassName('game-over-modal-buttons')[0];
 
-    if (element) {
-        if (document.getElementById('lichess-analysis-button')) {
-            return;
+        if (element) {
+            // Create the Lichess analysis button
+            button = createLichessAnalysisButton('lichess-game-over-analysis-button');
+
+            // Add the button to the game-over-modal-buttons element
+            const buttonContainer = document.createElement('div');
+            buttonContainer.className = 'game-over-review-button-component';
+            buttonContainer.appendChild(button);
+
+            element.insertBefore(buttonContainer, element.children[0]);
         }
-    
-        // Create the Lichess analysis button
-        button = document.createElement('button');
-        button.type = 'button';
-        button.id = 'lichess-analysis-button';
-        button.className = 'ui_v5-button-component ui_v5-button-primary ui_v5-button-large ui_v5-button-full';
-        button.innerHTML = '<span class="lichess-icon"></span><span> Analyse on Lichess</span>';
+    }
 
-        button.setAttribute('style', 'grid-column: span 2;');
+    // Add the Lichest Analysis button to other elements, if it doesn't exist yet
+    if (!document.getElementById('lichess-analysis-button')) {
+        var element = null;
+        if (element == null) {
+            element = document.getElementsByClassName('game-review-buttons-component')[0];
+        }
+        if (element == null) {
+            element = document.getElementsByClassName('game-review-buttons-focus-mode')[0];
+        }
+        if (element == null) {
+            element = document.getElementsByClassName('game-controls-view-component')[0];
+        }
+        if (element == null) {
+            element = document.getElementsByClassName('quick-analysis-buttons')[0];
+        }
 
-        button.addEventListener('click', openTab);
-    
-        // console.log("Button created, adding Lichess Analysis button... ");
-        try {
-            // Analysis button is added to different elements depending on the page
-            if (element.classList.contains('game-controls-view-component')) {
+        if (element) {
+            // Create the Lichess analysis button
+            button = createLichessAnalysisButton();
+
+            try {
                 const buttonContainer = document.createElement('div');
-                buttonContainer.className = 'game-controls-primary-component';
+                buttonContainer.className = 'game-review-buttons-review';
                 buttonContainer.appendChild(button);
 
-                element.insertBefore(buttonContainer, element.children[1]);
-            } else {
-                element.appendChild(button);
+                element.insertBefore(buttonContainer, element.children[0]);
+
+                // Add a separator
+                const separator = document.createElement('div');
+                separator.className = 'game-review-buttons-separator';
+                separator.innerHTML = '&nbsp;';
+                element.insertBefore(separator, element.children[1]);
+            } catch (e) {
+                console.log("Failed to add Lichess Analysis button.");
+                console.log(e);
             }
-            // console.log("Button added! :)");
-        } catch (e) {
-            console.log("Failed to add Lichess Analysis button.");
-            console.log(e);
         }
     }
 }
 
 const observer = new MutationObserver(onMutation)
 
-observer.observe(document.body, {childList: true, subtree: true})
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+})
